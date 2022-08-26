a17a8a9d5974a9d7cb283af527d9d941b510641b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2022-08-26 15:03:57 +0600

    Add files via upload

---
+++ color_palettes/data - Copy.js
@@ -0,0 +1,26 @@
+data = [
+    {
+        Project_name : "Abcd",
+        
+        used_colors : [
+            {"name" : "Aqua", "color" : "aqua"},  
+            {"name" : "Red", "color" : "red"},  
+            {"name" : "Blue", "color" : "blue"},  
+            {"name" : "Yellow", "color" : "yellow"},   
+        ]
+    
+    },
+    
+    {
+        Project_name : "Efgh",
+        
+        used_colors : [
+            {"name" : "Aqua", "color" : "aqua"},  
+            {"name" : "Red", "color" : "red"},  
+            {"name" : "Blue", "color" : "blue"},  
+            {"name" : "Yellow", "color" : "yellow"},   
+        ]
+    
+    },
+
+]
